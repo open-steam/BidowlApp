bf36298ea1637bdc1087acc335a2f47bc6890138
Dominik Niehus <dominik.niehus@coactum.de>
2013-11-13 12:48:03 +0100

    update app framework

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,10 +12,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new OpenSteam\BidWebdavBundle\OpenSteamBidWebdavBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
