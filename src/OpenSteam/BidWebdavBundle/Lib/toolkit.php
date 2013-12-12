50022f0b8e5041ad95c42e6d385bbd3106011558
Dominik Niehus <dominik.niehus@coactum.de>
2013-12-12 15:06:00 +0100

    replace url encoding in desc

--- src/OpenSteam/BidWebdavBundle/Lib/toolkit.php
+++ src/OpenSteam/BidWebdavBundle/Lib/toolkit.php
@@ -4,7 +4,7 @@ function getObjectName($steamObject)
 {
     $id = $steamObject->get_id();
     $name = $steamObject->get_attribute(OBJ_NAME);
-    $desc = $steamObject->get_attribute(OBJ_DESC);
+    $desc = urldecode($steamObject->get_attribute(OBJ_DESC));
     $identifier = $steamObject->get_identifier();
 
     if (!empty($desc) && $name != $desc) {
